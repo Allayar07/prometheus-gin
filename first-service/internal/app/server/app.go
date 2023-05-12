16d0a655147c1d3f5dc8d023f194f25043940800
Allayar07 <allayarhudaygulyyev14@gmail.com>
2023-05-12 19:31:08 +0500

    db connection

--- first-service/internal/app/server/app.go
+++ first-service/internal/app/server/app.go
@@ -1,20 +1,20 @@
 package server
 
 import (
+	"context"
 	"gin_prometheus/internal/app/handler"
 	"gin_prometheus/internal/repository"
 	"gin_prometheus/internal/service"
-	"github.com/jackc/pgx/v4/pgxpool"
 	"log"
 )
 
 func Init(port string) {
-	//db, err := repository.NewPostgresDB(context.Background())
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	db, err := repository.NewPostgresDB(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	repos := repository.NewRepository(&pgxpool.Pool{})
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
--- first-service/internal/repository/postgres.go
+++ first-service/internal/repository/postgres.go
@@ -1,15 +1,21 @@
 package repository
 
-//func NewPostgresDB(ctx context.Context) (*pgxpool.Pool, error) {
-//	dbURL := "postgres://" + "postgres" + ":" + url.QueryEscape("password0701") + "@" + "localhost" + ":" + "5432" + "/" + "practice" + "?sslmode=" + "disable"
-//	dbPool, err := pgxpool.Connect(ctx, dbURL)
-//
-//	if err != nil {
-//		return nil, err
-//	}
-//	err = dbPool.Ping(ctx)
-//	if err != nil {
-//		return nil, err
-//	}
-//	return dbPool, nil
-//}
+import (
+	"context"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"net/url"
+)
+
+func NewPostgresDB(ctx context.Context) (*pgxpool.Pool, error) {
+	dbURL := "postgres://" + "postgres" + ":" + url.QueryEscape("password0701") + "@" + "practice-db" + ":" + "5432" + "/" + "practice" + "?sslmode=" + "disable"
+	dbPool, err := pgxpool.Connect(ctx, dbURL)
+
+	if err != nil {
+		return nil, err
+	}
+	err = dbPool.Ping(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return dbPool, nil
+}
