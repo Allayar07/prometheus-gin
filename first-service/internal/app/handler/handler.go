43e0491c933db7d5c90ecffcf58573ce14ce4a88
Allayar07 <allayarhudaygulyyev14@gmail.com>
2023-05-13 18:00:41 +0500

    configuration grafana yml

--- first-service/internal/app/handler/handler.go
+++ first-service/internal/app/handler/handler.go
@@ -27,15 +27,16 @@ type Metric struct {
 	Args            []string
 }
 
-var reqCnt = &Metric{
+var reqSummary = &Metric{
 	ID:          "reqCnt",
 	Name:        "requests_total",
 	Description: "the number of HTTP requests processed",
 	Type:        "counter_vec",
-	Args:        []string{"status"}}
+	Args:        []string{"status"},
+}
 
 type Prometheus struct {
-	reqCnt        *prometheus.SummaryVec
+	reqSummary    *prometheus.SummaryVec
 	router        *gin.Engine
 	listenAddress string
 	Metric        *Metric
@@ -54,9 +55,10 @@ func (h *Handler) InitRoutes() *gin.Engine {
 
 func newPrometheus(subsystem string) *Prometheus {
 	p := &Prometheus{
-		Metric:        reqCnt,
+		Metric:        reqSummary,
 		MetricsPath:   "/metrics",
-		listenAddress: ":9911"}
+		listenAddress: ":9911",
+	}
 	p.registerMetrics(subsystem)
 	p.router = gin.Default()
 	return p
@@ -66,20 +68,20 @@ func (p *Prometheus) registerMetrics(subsystem string) {
 	metric := prometheus.NewSummaryVec(
 		prometheus.SummaryOpts{
 			Namespace:  "services",
-			Subsystem:  "http",
+			Subsystem:  subsystem,
 			Name:       "request",
 			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
 		},
-		reqCnt.Args,
+		reqSummary.Args,
 	)
 	if err := prometheus.Register(metric); err != nil {
-		logrus.Infof("%s could not be registered: ", reqCnt, err)
+		logrus.Infof("%s could not be registered: %s", reqSummary, err)
 	} else {
-		logrus.Infof("%s registered.", reqCnt)
+		logrus.Infof("%s registered.", reqSummary)
 	}
-	p.reqCnt = metric
+	p.reqSummary = metric
 
-	reqCnt.MetricCollector = metric
+	reqSummary.MetricCollector = metric
 }
 
 func (p *Prometheus) use(e *gin.Engine) {
@@ -98,7 +100,7 @@ func (p *Prometheus) handlerFunc() gin.HandlerFunc {
 
 		status := strconv.Itoa(c.Writer.Status())
 		finished := time.Since(start).Seconds()
-		p.reqCnt.WithLabelValues(status).Observe(finished)
+		p.reqSummary.WithLabelValues(status).Observe(finished)
 	}
 }
 
